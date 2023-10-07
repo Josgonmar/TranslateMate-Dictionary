bb2b92fd720fe116f2c98169ac32c7857ae8fe18
unknown <josgonmar14@gmail.com>
2023-10-07 20:48:05 +0200

    Print the size of the dictionary when loading from local, and dump it only when a change has been made. Also, some minor improvements to prompts and feedback

--- translate_mate.py
+++ translate_mate.py
@@ -10,26 +10,30 @@ class MainProgram:
     def __init__(self, file_path):
         self.file_path = file_path
         self.__data = []
+        self.__hasChanges = False
 
 
     def __save_and_exit(self):
-        with open(self.file_path, "w") as file:
-            json.dump(self.__data, file, indent=4)
-        print(Fore.GREEN + "Data saved successfully. Goodbye!")
+        if (self.__hasChanges):
+            with open(self.file_path, "w") as file:
+                json.dump(self.__data, file, indent=4)
+            print(Fore.GREEN + "Data saved successfully. Goodbye!")
+        else:
+            print(Fore.GREEN + "Goodbye!")
         sys.exit(0)
 
     def __load_data(self):
         try:
             with open(self.file_path, "r") as file:
                 self.__data = json.load(file)
-            print(Fore.GREEN + "Data loaded successfully.")
+                print(Fore.LIGHTGREEN_EX + f"[Successfully loaded a total of: '{len(self.__data)}' word(s) from local]")
         except FileNotFoundError:
-            print(Fore.YELLOW + "JSON file not found. Creating an empty dictionary.")
+            print(Fore.YELLOW + "[JSON file not found. Creating an empty dictionary locally]")
             with open(self.file_path, "w") as file:
                 json.dump(self.__data, file, indent=4)
 
     def __add_word(self):
-            
+
             def ask_prompt():
                 answer = input("Is it correct? (YES/no): ")
                 if answer.strip().upper() == "YES" or not answer:
@@ -45,10 +49,11 @@ class MainProgram:
 
             new_word = [english, german]
 
-            print(f"About to add '{english}' and its German translation '{german}' to the dictionary.")
+            print(f"So, you entered: '{english}' and its German translation '{german}' to the dictionary.")
 
             if (ask_prompt()):
                 self.__data.append(new_word)
+                self.__hasChanges = True
 
     def __display_menu(self):
         print(Fore.GREEN + "Please, select one of the available options down below:")
@@ -66,7 +71,7 @@ class MainProgram:
 
         self.__load_data()
 
-        print(Fore.GREEN + "Welcome to TranslateMate, an interactive EN-DE dictionary!")
+        print(Fore.GREEN + "Welcome to " + Fore.CYAN + "TranslateMate" + Fore.GREEN + ", an interactive EN-DE dictionary!")
 
         while True:
             self.__display_menu()
@@ -76,14 +81,11 @@ class MainProgram:
 
             if choice == '1':
                 self.__add_word()
-
             elif choice == '2':
-                print("You selected Option 2. It does nothing just yet!")
-
+                print("You selected Option 2, but it does nothing just yet!")
             elif choice == '3':
-                print("You selected Option 3. It does nothig just yet!")
-
-            elif choice == '4':
+                print("You selected Option 3, but it does nothig just yet!")
+            elif choice == '4' or choice.strip().lower() == 'q':
                 self.__save_and_exit()
                 break
             else:
